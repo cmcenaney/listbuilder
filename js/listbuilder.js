842d48ec3a1f05333ca5bc45e24b3b5fec5e69bb
Colleen McEnaney <colleenmcenaney@gmail.com>
2014-07-13 11:55:15 -0400

    fixed data format, added more lists to page, connected scoop button

--- js/listbuilder.js
+++ js/listbuilder.js
@@ -1,29 +1,11 @@
 
 
-
 var num = 0;
 
 function newItem(form){
+
+ 
     num ++;
- 
-
-    new_items = {};
-    
-    
-
-	$('.form_section').append(
-		'<form name="myform" action="" method="GET">' +
-            '<p>kicker <input TYPE="text" name="kicker" value="" class="kicker_form_box"></p>' +
-            '<p>headline <input TYPE="text" name="headline" value="" class="headline_form_box"></p>' +
-            '<p>body text <input type="text" name="body_text" value="" class="body_form_box"></p>' +
-            '<p>image url <input TYPE="text" name="img_url" value="" class="img_form_box"></p>' +
-            '<p>image caption <input TYPE="text" name="img_caption" value=""></p>' +
-            '<p>image credit <input TYPE="text" name="img_credit" value="" class="imgcredit_form_box"></p>' +
-            '<INPUT type="button" name="button" value="Add Item" onClick="newItem(this.form)">' +
-            '<INPUT type="button" name="button" value="Submit" onClick="testResults(this.form)">' +
-          '</form>'
-				
-	);
 
     var kicker = form.kicker.value;
     var headline = form.headline.value;
@@ -31,33 +13,7 @@ function newItem(form){
     var img_url = form.img_url.value;
     var img_caption = form.img_caption.value;
     var img_credit = form.img_credit.value;
-
-    new_items.push(kicker);
-    new_items.push(headline);
-    new_items.push(body_text);
-    new_items.push(img_url);
-    new_items.push(img_caption);
-    new_items.push(img_credit);
-
-    console.log(new_items);
-
-}
-
-function testResults (form) {
- 
-    var kicker = form.kicker.value;
-    var headline = form.headline.value;
-    var body_text = form.body_text.value;
-    var img_url = form.img_url.value;
-    var img_caption = form.img_caption.value;
-    var img_credit = form.img_credit.value;
     
-
-  
-
-
-    var source   = $("#some-template").html();
-    var template = Handlebars.compile(source);
     var data = { 
         lists: [{
             kicker: kicker,
@@ -69,10 +25,58 @@ function testResults (form) {
         }]
     };
 
+    testResults(data);
+
     console.log(data);
+   
+
+  
 
 
+    
+ 
+
+    // new_items = {};
+    
+    
+
+    $('.form_section').append(
+        '<form name="myform" action="" method="GET">' +
+            '<p>kicker <input TYPE="text" name="kicker" value="" class="kicker_form_box"></p>' +
+            '<p>headline <input TYPE="text" name="headline" value="" class="headline_form_box"></p>' +
+            '<p>body text <input type="text" name="body_text" value="" class="body_form_box"></p>' +
+            '<p>image url <input TYPE="text" name="img_url" value="" class="img_form_box"></p>' +
+            '<p>image caption <input TYPE="text" name="img_caption" value=""></p>' +
+            '<p>image credit <input TYPE="text" name="img_credit" value="" class="imgcredit_form_box"></p>' +
+            '<INPUT type="button" name="button" value="Add Item" onClick="newItem(this.form)">' +
+            '<INPUT type="button" name="button" value="Submit" onClick="testResults(this.form)">' +
+          '</form>'
+                
+    );
+
+    // var kicker = form.kicker.value;
+    // var headline = form.headline.value;
+    // var body_text = form.body_text.value;
+    // var img_url = form.img_url.value;
+    // var img_caption = form.img_caption.value;
+    // var img_credit = form.img_credit.value;
+
+    // new_items.push(kicker);
+    // new_items.push(headline);
+    // new_items.push(body_text);
+    // new_items.push(img_url);
+    // new_items.push(img_caption);
+    // new_items.push(img_credit);
+
+    // console.log(new_items);
+}
+
+function testResults (form, data) {
+    var source   = $("#some-template").html();
+    var template = Handlebars.compile(source);
     $("#content").html(template(data));
+
+    
 
 }
 
