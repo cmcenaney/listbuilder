95101edcffa3fb9350cbee2db3bd05959ade60c8
Colleen McEnaney <colleenmcenaney@gmail.com>
2014-07-17 22:49:14 -0400

    added extra export option

--- js/listbuilder-v1.js
+++ js/listbuilder-v1.js
@@ -1,9 +1,18 @@
+
+
+
+
+
+
 num = 0;
 data2 = {};
 new_items = [];
 data3 = {};
 
 lists = [];
+
+
+
 
 
 
@@ -46,7 +55,11 @@ function newItem(form){
     lists.push(data2);
    
     data3["lists"] = lists;
-    //console.log(data3);  
+    //console.log(data3); 
+
+    localStorage.setItem('data3', JSON.stringify(data3));
+
+
 
      
 
@@ -74,12 +87,39 @@ function addNew(){
 
 }
 
+function threeHundred(){
+    
+    
+    console.log('this');
+    var source   = $("#some-template").html();
+    var template = Handlebars.compile(source);
+    $("#content").html(template(data3));
+    
+    var retrievedObject = localStorage.getItem('data3'); 
+    console.log('retrievedObject: ', JSON.parse(retrievedObject));
+
+    $('#code_output').prepend(
+        '/* ARTICLE PAGE */'+
+        '&lt;style&gt;.interactive{border-top:0!important;border-bottom:0!important}.interactive.layout-sub-medium{width:540px;margin-left:73px}.list-wrapper{margin:20px 0 0 0px;display:inline-block}.list-item{margin:0 0 50px}.list-images{margin-right:5%;height:100%;float:left;width:45%}.list-images img{width:100%}.list-content{width:50%;float:left}.list-line{margin:0 0 30px;background-color:#000;height:1px}.list-kicker{font-size:11px;line-height:13px;letter-spacing:3px;font-weight:500;font-family:nyt-franklin,arial,helvetica,sans-serif;margin:0 0 8px;text-transform:uppercase}.list-headline{margin:0 0 12px;font-size:25px;font-weight:700;font-family:nyt-cheltenham,georgia,"times new roman",times,serif}.list-body{font-size:16px;line-height:23px;font-family:georgia,"times new roman",times,serif}.list-clear{clear:left}@media only screen and (max-width:768px){.list-wrapper{width:100%;margin:10px 0}.list-content{width:100%;float:none}.list-images{float:none;margin:0 0 20px}.list-line{display:none}}&lt;/style&gt;'
+    );
+
+}
+
 function testResults() { 
 
     var source   = $("#some-template").html();
     var template = Handlebars.compile(source);
     $("#content").html(template(data3));
     
+    var retrievedObject = localStorage.getItem('data3'); 
+    console.log('retrievedObject: ', JSON.parse(retrievedObject));
+
+   $('#code_output').prepend(
+        '/*FULL PAGE */'+
+        '&lt;style&gt;.interactive{border-top:0!important;border-bottom:0!important}.interactive.layout-sub-medium{width:540px;margin-left:73px}.list-wrapper{margin:20px 0 0 31px;display:inline-block}.list-item{margin:0 0 50px}.list-images{margin-right:5%;height:100%;float:left;width:45%}.list-images img{width:100%}.list-content{width:50%;float:left}.list-line{margin:0 0 30px;background-color:#000;height:1px}.list-kicker{font-size:11px;line-height:13px;letter-spacing:3px;font-weight:500;font-family:nyt-franklin,arial,helvetica,sans-serif;margin:0 0 8px;text-transform:uppercase}.list-headline{margin:0 0 12px;font-size:25px;font-weight:700;font-family:nyt-cheltenham,georgia,"times new roman",times,serif}.list-body{font-size:16px;line-height:23px;font-family:georgia,"times new roman",times,serif}.list-clear{clear:left}@media only screen and (max-width:768px){.list-wrapper{width:100%;margin:10px 0}.list-content{width:100%;float:none}.list-images{float:none;margin:0 0 20px}.list-line{display:none}}&lt;/style&gt;'
+
+
+   );
 
 
   
@@ -107,7 +147,9 @@ function testResults() {
 
 
 
-
+function addText() {
+    
+}
 
 
 
